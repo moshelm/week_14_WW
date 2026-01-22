f8445ac8b24a2fc2c91e2658f93d10ad832fda53
Moshe <moshe@example.com>
2026-01-22 09:29:48 +0200

    create Fastapi() object and basic main

---
+++ app/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from route import router
+import uvicorn
+
+app = FastAPI()
+app.include_router(router)
+
+@app.on_event('startup')
+def init_database():
+    print('connecting to database')
+
+if __name__=="__main__":
+    uvicorn.run(app,host='0.0.0.0',port=8000)
